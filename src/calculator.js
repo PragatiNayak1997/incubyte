a130592ea02ed8a9fdef99da7c2482ec278f51bf
PragatiNayak1997 <pragatinayak56@gmail.com>
2024-11-16 22:38:01 +0530

    Add test and logic for summing two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,11 @@
- function add(numbers) {
+function add(numbers) {
   if (!numbers) return 0;
 
-  if(numbers.length ===1) return Number(numbers);
+  if (numbers.length === 1) return Number(numbers);
+
+  const parts = numbers.split(",");
+
+  return parts.reduce((accumulator, number) => accumulator + Number(number), 0);
 }
 
-module.exports = {add}
+module.exports = { add };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,11 +1,15 @@
 import { add } from "../src/calculator.js";
 
 describe("String Calculator", () => {
-    test("returns 0 for an empty string", () => {
-      expect(add("")).toBe(0);
-    });
+  test("returns 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
 
-    test("returns the number itself for a single number", () => {
-      expect(add("1")).toBe(1);
-    });
-})  
+  test("returns the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  test("returns the sum of two numbers", () => {
+    expect(add("1,2")).toBe(3);
+  });
+});
