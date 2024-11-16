d013ea84a2a6e418c62de217e28e2283f47a036c
PragatiNayak1997 <pragatinayak56@gmail.com>
2024-11-16 19:28:39 +0530

    Add test for empty string input returning 0

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: "node",  // Use Node.js as the test environment
+  transform: {
+    "^.+\\.js$": "babel-jest", // Use Babel to transform .js files
+  },
+  verbose: true, // Show detailed test results
+};
+
+
